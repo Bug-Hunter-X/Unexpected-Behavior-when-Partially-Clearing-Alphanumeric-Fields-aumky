477a1489b4777fe433544fe2be23631fbf2061d9
BugHunter-X <askzenai@gmail.com>
2025-02-03 10:28:05 +0100

    Add bugSolution.cob file

---
+++ bugSolution.cob
@@ -0,0 +1,7 @@
+01  WS-AREA-1.                    PIC X(100). 
+01  WS-AREA-2.                    PIC X(100). 
+* Some other codes... 
+MOVE SPACE TO WS-AREA-1. 
+MOVE WS-AREA-1 TO WS-AREA-2. 
+* Some other codes...
+MOVE SPACE TO WS-AREA-1.
